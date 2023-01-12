4908575e7c718f4e0a8599977f93dc6f216bca62
Michael Ge <54512113+michaelge21@users.noreply.github.com>
2023-01-12 14:35:23 -0600

    bump

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ function postTodo() {
 }
 
 const submitButton = document.getElementById("submit");
+const inputField = document.getElementById("input");
+
+console.log(inputField);
+
 
 submitButton.addEventListener("click", () => {
-    console.log('ayyyy wassup (from the event listener)');
+    console.log(`ayyyy wassup (from the event listener). ${inputField.value}`);
 })
